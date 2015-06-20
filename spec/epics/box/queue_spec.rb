69127442209fcff81d3b68bd7901851385934f97
Maximilian Schulz <m.schulz@kulturfluss.de>
2015-06-20 14:20:50 +0200

    Add specs for new helper queue methods

--- spec/epics/box/queue_spec.rb
+++ spec/epics/box/queue_spec.rb
@@ -2,9 +2,72 @@ module Epics
   module Box
     RSpec.describe Queue do
 
+      let(:client) { described_class.client }
+
       describe '.client' do
         it 'returns an instance of a beanstalk client' do
           expect(described_class.client).to be_an_instance_of(Beaneater)
+        end
+      end
+
+      describe '.check_processing_status' do
+        let(:tube) { client.tubes[Queue::STA_TUBE] }
+
+        before { tube.clear }
+
+        it 'puts a new message onto the STA queue' do
+          expect { described_class.check_processing_status }.to change { tube.peek(:ready) }
+        end
+
+        it 'puts only the provided account id onto the job' do
+          described_class.check_processing_status(1)
+          expect(tube.peek(:ready).body).to eq(account_ids: [1])
+        end
+
+        it 'puts all provided account ids onto the job' do
+          described_class.check_processing_status([1, 2])
+          expect(tube.peek(:ready).body).to eq(account_ids: [1, 2])
+        end
+
+        it 'puts all existing account ids onto the job if none is provided' do
+          accounts = Array.new(3).map { Account.create }
+          described_class.check_processing_status
+          expect(tube.peek(:ready).body).to eq(account_ids: accounts.map(&:id))
+        end
+      end
+
+      describe '.check_accounts' do
+        let(:tube) { client.tubes[Queue::ORDER_TUBE] }
+
+        before { tube.clear }
+
+        context 'no job currently queued' do
+          it 'puts a new message onto the check orders queue' do
+            expect { described_class.check_accounts }.to change { tube.peek(:delayed) }
+          end
+
+          it 'puts only the provided account id onto the job' do
+            described_class.check_accounts(1)
+            expect(tube.peek(:delayed).body).to match hash_including(account_ids: [1])
+          end
+
+          it 'puts all provided account ids onto the job' do
+            described_class.check_accounts([1, 2])
+            expect(tube.peek(:delayed).body).to match hash_including(account_ids: [1, 2])
+          end
+
+          it 'puts all existing account ids onto the job if none is provided' do
+            accounts = Array.new(3).map { Account.create }
+            described_class.check_accounts
+            expect(tube.peek(:delayed).body).to match hash_including(account_ids: accounts.map(&:id))
+          end
+        end
+
+        context 'job already queued' do
+          it 'does not queue another job' do
+            described_class.check_accounts
+            expect { described_class.check_accounts }.to_not change { tube.peek(:delayed).id }
+          end
         end
       end
 
