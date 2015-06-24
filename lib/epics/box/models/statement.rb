30895b6c521086d69ee231ffa3961d061c3d0d85
Lars Brillert <lars@railslove.com>
2015-06-24 09:07:29 +0200

    the newest will be the top

--- lib/epics/box/models/statement.rb
+++ lib/epics/box/models/statement.rb
@@ -1,7 +1,7 @@
 class Epics::Box::Statement < Sequel::Model
   def self.paginated_by_account(account_id, options = {})
     options = { per_page: 10, page: 1 }.merge(options)
-    where(account_id: account_id).limit(options[:per_page]).offset((options[:page] - 1) * options[:per_page])
+    where(account_id: account_id).limit(options[:per_page]).offset((options[:page] - 1) * options[:per_page]).reverse_order(:date)
   end
 
   def credit?
