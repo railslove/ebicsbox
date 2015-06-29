6a046da6a7c5377a7e6f5f6da9d119db932b9a11
Lars Brillert <lars@railslove.com>
2015-06-29 11:19:36 +0200

    otherwise jrubyc will screw things up :(

--- lib/epics/box/server.rb
+++ lib/epics/box/server.rb
@@ -1,14 +1,14 @@
-class Unique < Grape::Validations::Base
-  def validate_param!(attr_name, params)
-    unless DB[:transactions].where(attr_name => params[attr_name]).count == 0
-      raise Grape::Exceptions::Validation, params: [@scope.full_name(attr_name)], message: "must be unique"
-    end
-  end
-end
-
 module Epics
   module Box
     class Server < Grape::API
+      class Unique < Grape::Validations::Base
+        def validate_param!(attr_name, params)
+          unless DB[:transactions].where(attr_name => params[attr_name]).count == 0
+            raise Grape::Exceptions::Validation, params: [@scope.full_name(attr_name)], message: "must be unique"
+          end
+        end
+      end
+
       format :json
 
       helpers do
