dfdd9f606c0db981f84df9a4f1a8b337527583d6
Maximilian Schulz <m.schulz@kulturfluss.de>
2015-07-04 00:20:25 +0200

    Create a basic header component

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -2,10 +2,21 @@ require("bootstrap-webpack");
 require("./styles/custom.css");
 
 var React = require('react');
+import Navigation from './components/navigation.jsx';
 
 class TestApp extends React.Component {
   render() {
-    return <h1>Hello World!</h1>;
+    return(
+      <div>
+        <Navigation />
+        <div className="container" role="main">
+          <div className="row">
+            <div className="col-xs-6">Hello</div>
+            <div className="col-xs-6">World</div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
