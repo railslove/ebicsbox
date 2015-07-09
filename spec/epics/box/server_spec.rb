f525a19ebeb64bbfe31f272d1135656dcd686eb1
Maximilian Schulz <m.schulz@kulturfluss.de>
2015-07-09 13:21:20 +0200

    Remove format specification from server spec as all endpoints return json

--- spec/epics/box/server_spec.rb
+++ spec/epics/box/server_spec.rb
@@ -5,19 +5,19 @@ module Epics
         let(:account) { Account.create(iban: SecureRandom.uuid) }
 
         it 'returns an empty array for new accounts' do
-          get "#{account.iban}/statements", format: 'json'
+          get "#{account.iban}/statements"
           expect_json([])
         end
 
         it 'returns properly formatted statements' do
           statement = Statement.create account_id: account.id
-          get "#{account.iban}/statements", format: 'json'
+          get "#{account.iban}/statements"
           expect_json '0.statement', { account_id: account.id }
         end
 
         it 'passes page and per_page params to statement retrieval function' do
           allow(Statement).to receive(:paginated_by_account) { double(all: [])}
-          get "#{account.iban}/statements?page=4&per_page=2", format: 'json'
+          get "#{account.iban}/statements?page=4&per_page=2"
           expect(Statement).to have_received(:paginated_by_account).with(account.id, per_page: 2, page: 4)
         end
 
