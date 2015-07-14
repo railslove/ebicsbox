9cfbe003c15761ca785b37945ee7e338299b3f5a
Maximilian Schulz <m.schulz@kulturfluss.de>
2015-07-14 12:01:00 +0200

    Extend text input component to render help messages and set required attributes

--- frontend/common/text_input.jsx
+++ frontend/common/text_input.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 
 class TextInput extends React.Component {
   render() {
+    var help;
+    var helpId;
+    if(this.props.help) {
+      helpId = `help-for-${this.props.for}`;
+      help = <span id={helpId} className="help-block">{this.props.help}</span>
+    }
+
     return(
       <div className="form-group">
         <label htmlFor={this.props.for}>{this.props.label}</label>
-        <input type="text" id={this.props.for} ref={this.props.for} name={this.props.for} className="form-control" value={this.props.value} onChange={this.props.onChange} />
+        <input type="text" id={this.props.for} ref={this.props.for} name={this.props.for} required={this.props.required} className="form-control" value={this.props.value} onChange={this.props.onChange} aria-describedby={helpId} />
+        {help}
       </div>
     )
   }
