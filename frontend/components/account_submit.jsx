c387cfb467d8741d67b399b40a2d3fdbac278341
Maximilian Schulz <m.schulz@kulturfluss.de>
2015-07-15 21:37:25 +0200

    Display ini letter on waiting screen and add a print button

--- frontend/components/account_submit.jsx
+++ frontend/components/account_submit.jsx
@@ -9,6 +9,7 @@ class AccountSubmit extends React.Component {
     this.state = { account: {} }
 
     this.onSubmit = this.onSubmit.bind(this);
+    this.printLetter = this.printLetter.bind(this);
   }
 
   componentWillMount() {
@@ -43,19 +44,38 @@ class AccountSubmit extends React.Component {
     )
   }
 
+  printLetter(e) {
+    e.preventDefault();
+    var printFrame = window.frames["iniletter-frame"];
+    printFrame.focus();
+    printFrame.print();
+  }
+
+  componentDidUpdate() {
+    var printFrame = window.frames["iniletter-frame"];
+    printFrame.document.write(`<body>${this.state.account.ini_letter}</body>`);
+  }
+
   renderWaiting() {
     let submittedAt = new Date(this.state.account.submitted_at);
+    let iframeContents = "data:text/html;charset=utf-8," + escape(this.state.account.ini_letter);
     return(
       <div className="container">
         <div className="row">
           <div className="col-sm-6">
             <h3>Finish account setup</h3>
             <Info type="warning">
-              EBICS setup data has been submitted on {submittedAt.toLocaleDateString()}. The
-              system checks against the bank's servers on a daily basis.
-              As soon as the bank activates the EBICS account on their side, the system will update
-              its status.
+              EBICS setup data has been submitted on {submittedAt.toLocaleDateString()}. Please print
+              the following INI letter and send it to your bank, so they can activate your account.
+              The EBICS::Box performs an activation check on a daily basis. As soon as the bank
+              activates the EBICS account on their side, the system will update its status automatically.
             </Info>
+
+            <iframe id="iniletter-frame" name="iniletter-frame" style={{width: '100%', border: '1px solid #ddd'}}>
+              <p>Your browser does not support iframes.</p>
+            </iframe>
+
+            <button onClick={this.printLetter} className="btn btn-primary">print</button>
           </div>
         </div>
       </div>
