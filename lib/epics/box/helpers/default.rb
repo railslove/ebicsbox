95a822afbe77172c38cd8e34139de319479a0639
Maximilian Schulz <m.schulz@kulturfluss.de>
2016-01-20 11:37:01 +0100

    Refactor the way the access token is loaded and add some specs for it

--- lib/epics/box/helpers/default.rb
+++ lib/epics/box/helpers/default.rb
@@ -1,15 +1,16 @@
+require_relative '../models/user'
+require_relative '../server'
+
 module Epics
   module Box
     module Helpers
       module Default
+        def access_token
+          params['access_token'] || headers['Authorization'].to_s[/token (.+)/, 1]
+        end
+
         def current_user
-          @current_user ||= begin
-            if token = params['access_token'] || headers['Authorization'].to_s[/token (.+)/, 1]
-              User.find_by_access_token(token)
-            else
-              nil
-            end
-          end
+          @current_user ||= User.find_by_access_token(access_token)
         end
 
         def current_organization
--- lib/epics/box/models/user.rb
+++ lib/epics/box/models/user.rb
@@ -5,6 +5,7 @@ class Epics::Box::User < Sequel::Model
   one_to_many :subscribers
 
   def self.find_by_access_token(access_token)
+    return unless access_token
     first(access_token: access_token)
   end
 end
