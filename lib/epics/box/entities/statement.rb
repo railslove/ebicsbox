8f9bfa42f726d9bbbb10a9fc64dea61495ca3c9f
Maximilian Schulz <m.schulz@kulturfluss.de>
2016-01-28 14:57:26 +0100

    Update documentation on statement fields

--- lib/epics/box/entities/statement.rb
+++ lib/epics/box/entities/statement.rb
@@ -10,15 +10,15 @@ module Epics
         expose :bic
         expose :iban
         expose :type
-        expose :amount
+        expose :amount, documentation: { type: "Integer", desc: "Amount in cents" }
         expose :date
-        expose(:remittance_information) { |statement| statement[:svwz] || statement[:information] }
-        expose :eref
-        expose :mref
-        expose :reference
+        expose(:remittance_information, documentation: { type: "String", desc: "Wire transfer reference" }) { |statement| statement[:svwz] || statement[:information] }
+        expose :eref, documentation: { type: "String", desc: "SEPA end-to-end reference" }
+        expose :mref, documentation: { type: "String", desc: "SEPA mandate reference" }
+        expose :reference, documentation: { type: "String", desc: "Additional references (like customer reference, etc.)" }
         expose :bank_reference
-        expose :creditor_identifier
-        expose :swift_code, as: :transaction_type
+        expose :creditor_identifier, documentation: { type: "String", desc: "SEPA creditor identifier" }
+        expose :swift_code, as: :transaction_type, documentation: { type: "String", desc: "SWIFT transaction code" }
         expose(:_links, documentation: { type: "Hash", desc: "Links to resources" }) do |statement|
           iban = statement.account.iban
           trx = statement.transaction
