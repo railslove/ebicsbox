7d954be0349a7730f9c041fad03f965a055996fb
Johannes Heck <yoyostile@gmail.com>
2016-02-22 14:36:00 +0100

    [wip] blebics for ebics-box

--- vendor/cache/cmxl-5fcc52206f2f/lib/cmxl/fields/closing_balance.rb
+++
@@ -1,9 +0,0 @@
-require 'cmxl/fields/account_balance'
-module Cmxl
-  module Fields
-    class ClosingBalance < AccountBalance
-      self.tag = 62
-      self.parser = superclass.parser
-    end
-  end
-end
--- vendor/cache/cmxl-5fcc52206f2f/lib/cmxl/version.rb
+++
@@ -1,3 +0,0 @@
-module Cmxl
-  VERSION = "0.1.3"
-end
