90c2feade91c1b677ec476917fc97adda9146d3d
Maximilian Schulz <m.schulz@kulturfluss.de>
2016-03-22 16:24:42 +0100

    Migrate old she data and expose a unique statement id
    The unique id has been requested by customers, so they can make sure that data is not imported twice into their system.

---
+++ migrations/20160322132000_migrate_statement_shas.rb
@@ -0,0 +1,26 @@
+require_relative '../lib/epics/box'
+
+require_relative '../lib/epics/box/models/bank_statement'
+require_relative '../lib/epics/box/models/statement'
+require_relative '../lib/epics/box/business_processes/import_bank_statement'
+require_relative '../lib/epics/box/business_processes/import_statements'
+
+Sequel.migration do
+  up do
+    # Delete all old statements
+    Epics::Box::Statement.where("bank_statement_id IS NOT NULL").destroy
+
+    # Re-Import all statements
+    # Rebuild statements from bank statements
+    Epics::Box::BankStatement.all.each do |bank_statement|
+      Epics::Box::BusinessProcesses::ImportStatements.from_bank_statement(bank_statement)
+    end
+
+    add_column :statements, :public_id, :uuid, default: Sequel.function(:uuid_generate_v4)
+  end
+
+  down do
+    # Digest::SHA2.hexdigest([transaction.sha, transaction.date, transaction.amount_in_cents, transaction.sepa].join).to_s,
+    drop_column :statements, :public_id
+  end
+end
