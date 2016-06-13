7868f2939c4395349e34537824d2c511e6906112
Johannes Heck <yoyostile@gmail.com>
2016-06-13 18:10:07 +0200

    fill missing webhooks

--- migrations/20160613154542_add_webhook_token_null_constraint.rb
+++ migrations/20160613154542_add_webhook_token_null_constraint.rb
@@ -1,7 +1,20 @@
+require 'SecureRandom'
+require_relative '../box/models/organization'
+
 Sequel.migration do
-  change do
+  up do
+    Box::Organization.where('webhook_token IS NULL or webhook_token = ?', '').each do |orga|
+      orga.webhook_token = SecureRandom.hex
+      orga.savegst
+    end
     alter_table(:organizations) do
       set_column_not_null :webhook_token
+    end
+  end
+
+  down do
+    alter_table(:organizations) do
+      set_column_allow_null :webhook_token
     end
   end
 end
