ba78487b75b6be0711ed7eea9856b7f4e44af1ef
Lars Brillert <lars@railslove.com>
2017-09-21 16:57:00 +0200

    actually it doesn’t make sense to rescue here

--- migrations/20170921121419_mange_keys.rb
+++ migrations/20170921121419_mange_keys.rb
@@ -7,7 +7,7 @@ Sequel.migration do
         Epics::Client.new( StringIO.new(sub[:encryption_keys]) , ENV['PASSPHRASE'],'', '', '', '')
       rescue OpenSSL::Cipher::CipherError
         begin
-          client = Epics::Client.new( StringIO.new(sub[:encryption_keys]) , 'aff072af1329d610b79f1105c899d280' ,'', '', '', '') rescue nil
+          client = Epics::Client.new( StringIO.new(sub[:encryption_keys]) , 'aff072af1329d610b79f1105c899d280' ,'', '', '', '')
           client.passphrase= ENV['PASSPHRASE']
 
           self[:subscribers].where(id: sub[:id]).update(encryption_keys: client.send(:dump_keys))
