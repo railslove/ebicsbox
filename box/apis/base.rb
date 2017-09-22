632fdbc9c5f90d953072161b5d1002fedde157e3
Lars Brillert <lars@railslove.com>
2017-09-22 11:14:35 +0200

    revert API order switch, provide docs for v2 and v1

--- box/apis/base.rb
+++ box/apis/base.rb
@@ -6,8 +6,8 @@ require_relative './v2/base'
 module Box
   module Apis
     class Base < Grape::API
-      mount Box::Apis::V2::Base
       mount Box::Apis::V1::Base
+      mount Box::Apis::V2::Base
     end
   end
 end
--- box/apis/v2/base.rb
+++ box/apis/v2/base.rb
@@ -22,54 +22,56 @@ module Box
         mount Events
 
         add_swagger_documentation \
+          doc_version: 'v2',
+          mount_path: '/swagger_doc',
           info: {
-            title: "EBICS::Box",
-            contact_name: "Railslove GmbH",
-            contact_email: "ebics-box@railslove.com",
-            contact_url: "https://www.ebicsbox.com/",
-            description: <<-USAGE.strip_heredoc
-            A modern API for bank accounts. Fully automatize processing of incoming and outgoing money transactions. It enables high-level access to some EBCIS features and wrapps them with further functinality.
-            ## Clarification of terms
-            ### EREF
-            The most important building block of the EBICS::BOX is the EREF aka "End to End ID" or "End to End Reference". It is a universal identifier that will be used to recognize transactions throughout their whole lifecycle. The maximum length is 35 characters.
-            ### Matchmaking
-            Every time a new "outgoing" transaction is created (debit or credit) the EREF will be stored onthe internal watchlist, whenever we're seeing these IDs in new transactions you'll get notifiedvia Webhooks. The most used use case will be to identify chargebacks or detect that the moneywas actullay transfered from your bank account.
-            ### Media Types
-            All actions require and return JSON formatted data. Timestamps are always formatted using ISO 8601. All data is UTF-8 encoded.
+        title: "EBICS::Box",
+        contact_name: "Railslove GmbH",
+        contact_email: "ebics-box@railslove.com",
+        contact_url: "https://www.ebicsbox.com/",
+        description: <<-USAGE.strip_heredoc
+        A modern API for bank accounts. Fully automatize processing of incoming and outgoing money transactions. It enables high-level access to some EBCIS features and wrapps them with further functinality.
+        ## Clarification of terms
+        ### EREF
+        The most important building block of the EBICS::BOX is the EREF aka "End to End ID" or "End to End Reference". It is a universal identifier that will be used to recognize transactions throughout their whole lifecycle. The maximum length is 35 characters.
+        ### Matchmaking
+        Every time a new "outgoing" transaction is created (debit or credit) the EREF will be stored onthe internal watchlist, whenever we're seeing these IDs in new transactions you'll get notifiedvia Webhooks. The most used use case will be to identify chargebacks or detect that the moneywas actullay transfered from your bank account.
+        ### Media Types
+        All actions require and return JSON formatted data. Timestamps are always formatted using ISO 8601. All data is UTF-8 encoded.
 
-                Content-Type: application/json
+            Content-Type: application/json
 
-            ### Errors
+        ### Errors
 
-            Errors due to its REST nature, the API returns proper http error codes. Usually status codes in the 2xx range indicate a successful operation, 4xx indicates an error resulting from the provided attributes. And errors in the 5xx range indicate a problem in the EBICS::BOX. The JSON object returned looks like the following:
+        Errors due to its REST nature, the API returns proper http error codes. Usually status codes in the 2xx range indicate a successful operation, 4xx indicates an error resulting from the provided attributes. And errors in the 5xx range indicate a problem in the EBICS::BOX. The JSON object returned looks like the following:
 
-                {
-                  "message": "Human readable description of the error",
-                  "errors": {
-                    "<field>": [ "some error", "another error" ]
-                  }
-                }
+            {
+              "message": "Human readable description of the error",
+              "errors": {
+                "<field>": [ "some error", "another error" ]
+              }
+            }
 
-            ### Versioning
+        ### Versioning
 
-            If not specified otherwise, the API will always use the most recent version available. In order to use a specific version, clients need to request it via header:
+        If not specified otherwise, the API will always use the most recent version available. In order to use a specific version, clients need to request it via header:
 
-            ```Accept: application/vnd.ebicsbox.v2+json```.
+        ```Accept: application/vnd.ebicsbox-v2+json```.
 
-            Please note that we expect applications to be flexible enought to accept additional fields without a major version change. Breaking changes, like changed behaviour and removal or renaming of fields will always result in a version number bump.
+        Please note that we expect applications to be flexible enought to accept additional fields without a major version change. Breaking changes, like changed behaviour and removal or renaming of fields will always result in a version number bump.
 
-            ### Prerequisites
+        ### Prerequisites
 
-            To use every feature that is offered by the EBICS::BOX you should make sure that your bank supports and offers the respective order types.
+        To use every feature that is offered by the EBICS::BOX you should make sure that your bank supports and offers the respective order types.
 
-            * Transaction Import - `STA` or `C53`
-            * Usage protocols - `HAC`
-            * Credits - `CCT`
-            * Debits - `CDD` or `B2B`
+        * Transaction Import - `STA` or `C53`
+        * Usage protocols - `HAC`
+        * Credits - `CCT`
+        * Debits - `CDD` or `B2B`
 
-            Furthermore to process direct debits you'll have to obtain a Creditor Identification Number from the [Bundesbank](http://www.bundesbank.de/Navigation/DE/Aufgaben/Unbarer_Zahlungsverkehr/SEPA/Glaeubiger_Identifikationsnummer/glaeubiger_identifikationsnummer.html) and sign some additional contracts with your bank.
-            USAGE
-          }
+        Furthermore to process direct debits you'll have to obtain a Creditor Identification Number from the [Bundesbank](http://www.bundesbank.de/Navigation/DE/Aufgaben/Unbarer_Zahlungsverkehr/SEPA/Glaeubiger_Identifikationsnummer/glaeubiger_identifikationsnummer.html) and sign some additional contracts with your bank.
+        USAGE
+      }
       end
     end
   end
