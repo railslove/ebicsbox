1876794ade080df15a8351bb59ce08304b6ea7ee
Maxim <maxim@railslove.com>
2018-08-28 16:49:19 +0200

    improves default dataset migration

--- migrations/20180323132359_add_default_user_and_ogranisation.rb
+++ migrations/20180323132359_add_default_user_and_ogranisation.rb
@@ -1,6 +1,8 @@
 Sequel.migration do
   up do
-    orga_id = self[:organizations].insert(name: 'Primary Organization', webhook_token: SecureRandom.hex(24)) unless self[:organizations].any?
-    self[:users].insert(organization_id: orga_id, name: 'Primary user', admin: true) unless self[:users].any?
+    self[:organizations].insert(name: 'Primary Organization', webhook_token: SecureRandom.hex(32)) unless self[:organizations].any?
+
+    orga_id = self[:organizations].first[:id]
+    self[:users].insert(organization_id: orga_id, name: 'Primary user', admin: true, access_token: SecureRandom.hex(32)) unless self[:users].any?
   end
 end
