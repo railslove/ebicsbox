a2b15e05924c8f5f906d4d2291d9e35980ad6bbe
Maxim <maxim@railslove.com>
2018-11-14 15:12:29 +0100

    makes initial migration create users when none exist
    - for sure this time

--- migrations/20180323132359_add_default_user_and_ogranisation.rb
+++ migrations/20180323132359_add_default_user_and_ogranisation.rb
@@ -2,7 +2,10 @@ Sequel.migration do
   up do
     self[:organizations].insert(name: 'Primary Organization', webhook_token: SecureRandom.hex(32)) unless self[:organizations].any?
 
-    orga_id = self[:organizations].first[:id]
-    self[:users].insert(organization_id: orga_id, name: 'Primary user', admin: true, access_token: SecureRandom.hex(32)) unless self[:users].any?
+    if self[:users].empty?
+      orga_id = self[:organizations].first[:id]
+      token = SecureRandom.hex(32)
+      self[:users].insert(organization_id: orga_id, name: 'Primary user', admin: true, access_token: token)
+    end
   end
 end
