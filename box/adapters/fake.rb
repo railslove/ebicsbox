ea5e42a25f927d82a4ff0c2fa2b5e76da545bc34
Max <Uepsilon@users.noreply.github.com>
2019-07-02 16:12:25 +0200

    enables fake adapter to create direct debits (#190)

--- box/adapters/fake.rb
+++ box/adapters/fake.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'nokogiri'
 
 module Box
@@ -10,11 +12,11 @@ module Box
       end
 
       def self.setup(*args)
-        return new(*args)
+        new(*args)
       end
 
-      def ini_letter(name)
-        "Here would be the INI letter you would need to sign for your bank."
+      def ini_letter(_name)
+        'Here would be the INI letter you would need to sign for your bank.'
       end
 
       def INI
@@ -30,44 +32,36 @@ module Box
       end
 
       def dump_keys
-        "{}"
+        '{}'
       end
 
-      def STA(from = nil, to = nil)
+      def STA(_from = nil, _to = nil)
         # Create a random number of statements every day
-        ::File.read( ::File.expand_path("~/sta.mt940"))
+        ::File.read(::File.expand_path('~/sta.mt940'))
       end
 
-      def VMK(from = nil, to = nil)
+      def VMK(_from = nil, _to = nil)
         # Create a random number of statements every day
-        ::File.read( ::File.expand_path("~/vmk.mt942"))
+        ::File.read(::File.expand_path('~/vmk.mt942'))
       end
 
-      def HAC(from = nil, to = nil)
-        ::File.open( ::File.expand_path("~/hac_empty.xml"))
+      def HAC(_from = nil, _to = nil)
+        ::File.open(::File.expand_path('~/hac_empty.xml'))
       end
 
       def HTD
-        ::File.open( ::File.expand_path("~/htd.xml"))
+        ::File.open(::File.expand_path('~/htd.xml'))
       end
 
       def CD1(pain)
-        ["TRX#{SecureRandom.hex(6)}", "N#{SecureRandom.hex(6)}"]
-      end
-      alias :CDD :CD1
-      alias :CDB :CD1
-
-      def CCT(pain)
         doc = Nokogiri::XML(pain)
-        ["TRX#{SecureRandom.hex(6)}", "N#{SecureRandom.hex(6)}"]
-        trx = doc.css("Document CstmrCdtTrfInitn PmtInf CdtTrfTxInf")
-        eref = trx.css("PmtId EndToEndId").text
-        desc = trx.css("RmtInf Ustrd").text
-        amount = trx.css("Amt InstdAmt").text.gsub(/\./, '').to_i
+        trx = doc.css('Document CstmrDrctDbtInitn PmtInf DrctDbtTxInf')
+        eref = trx.css('PmtId EndToEndId').text
+        amount = trx.css('InstdAmt').text.gsub(/\./, '').to_i
         transaction = Transaction[eref: eref]
-        transaction.update_status("debit_received", reason: "Auto accept fake credit transfers")
+        transaction.update_status('credit_received', reason: 'Auto accept fake direct debit')
 
-        statement = Statement.create({
+        statement = Statement.create(
           account_id: transaction.account_id,
           sha: Digest::SHA2.hexdigest(SecureRandom.hex(12)).to_s,
           date: Date.today,
@@ -75,22 +69,58 @@ module Box
           amount: amount,
           sign: 1,
           debit: false,
-          swift_code: "",
+          swift_code: '',
+          reference: 'NOREF',
+          bank_reference: '',
+          bic: trx.css('DbtrAgt FinInstnId BIC').text,
+          iban: trx.css('DbtrAcct Id IBAN').text,
+          name: trx.css('Dbtr Nm').text,
+          information: 'Fake Direct Debit',
+          description: 'Fake Direct Debit',
+          eref: eref
+        )
+
+        Event.statement_created(statement)
+
+        ["TRX#{SecureRandom.hex(6)}", "N#{SecureRandom.hex(6)}"]
+      end
+      alias CDD CD1
+      alias CDB CD1
+
+      def CCT(pain)
+        doc = Nokogiri::XML(pain)
+        ["TRX#{SecureRandom.hex(6)}", "N#{SecureRandom.hex(6)}"]
+        trx = doc.css('Document CstmrCdtTrfInitn PmtInf CdtTrfTxInf')
+        eref = trx.css('PmtId EndToEndId').text
+        desc = trx.css('RmtInf Ustrd').text
+        amount = trx.css('Amt InstdAmt').text.gsub(/\./, '').to_i
+        transaction = Transaction[eref: eref]
+        transaction.update_status('debit_received', reason: 'Auto accept fake credit transfers')
+
+        statement = Statement.create(
+          account_id: transaction.account_id,
+          sha: Digest::SHA2.hexdigest(SecureRandom.hex(12)).to_s,
+          date: Date.today,
+          entry_date: Date.today,
+          amount: amount,
+          sign: 1,
+          debit: false,
+          swift_code: '',
           reference: desc,
-          bank_reference: "",
-          bic: trx.css("CdtrAgt FinInstnId BIC").text,
-          iban: trx.css("CdtrAcct Id IBAN").text,
-          name: trx.css("Cdtr Nm").text,
+          bank_reference: '',
+          bic: trx.css('CdtrAgt FinInstnId BIC').text,
+          iban: trx.css('CdtrAcct Id IBAN').text,
+          name: trx.css('Cdtr Nm').text,
           information: desc,
           description: desc,
-          eref: eref,
-        })
+          eref: eref
+        )
 
         Event.statement_created(statement)
         ["TRX#{SecureRandom.hex(6)}", "N#{SecureRandom.hex(6)}"]
       end
 
-      def AZV(dtazv)
+      def AZV(_dtazv)
         ["TRX#{SecureRandom.hex(6)}", "N#{SecureRandom.hex(6)}"]
       end
     end
