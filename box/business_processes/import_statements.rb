32460c38f43af48bba6bd2d87616fd496beff995
Maxim <maxim@railslove.com>
2019-07-09 15:20:42 +0200

    reduces logging for fetch statement job
    - already imported statements are debug info now

--- box/business_processes/import_statements.rb
+++ box/business_processes/import_statements.rb
@@ -34,7 +34,7 @@ module Box
         trx = statement_attributes_from_bank_transaction(bank_transaction, bank_statement)
 
         if (statement = account.statements_dataset.first(sha: trx[:sha]))
-          Box.logger.info("[BusinessProcesses::ImportStatements] Already imported. sha='#{statement.sha}'")
+          Box.logger.debug("[BusinessProcesses::ImportStatements] Already imported. sha='#{statement.sha}'")
           statement.update(settled: true) unless upcoming
           false
         else
