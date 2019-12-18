ab1c86954a0430fe8f5225ccb7ae662eba1b6e59
Maxim <maxim@railslove.com>
2019-12-18 08:40:40 +0100

    setting indizes is hard

--- migrations/20191217114900_recalculate_statement_sha.rb
+++ migrations/20191217114900_recalculate_statement_sha.rb
@@ -3,10 +3,7 @@
 Sequel.migration do
   up do
     alter_table :statements do
-      drop_index :sha
-
       rename_column :sha, :sha2
-      add_index :sha2, unique: true
 
       add_column :sha, :text
       add_index :sha, unique: true
@@ -21,8 +18,6 @@ Sequel.migration do
   down do
     alter_table :statements do
       drop_column :sha
-      drop_index :sha
-      drop_index :sha2
 
       rename_column :sha2, :sha
       add_index :sha, unique: true
