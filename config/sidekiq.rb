6efd32ffcb1ef2272b7023772b4770f1b8157f11
Johnny Park <johnnypark@mailbox.org>
2020-07-17 13:24:23 +0200

    - Adds auth support for Redis - Adds doc about Redis with SSL

--- config/sidekiq.rb
+++ config/sidekiq.rb
@@ -45,7 +45,15 @@ if ENV['SENTRY_DSN']
   end
 end
 
+redis_conf = { url: ENV['REDIS_URL'], password:  ENV['REDIS_PASSWORD'] }
+
+Sidekiq.configure_client do |config|
+  config.redis = redis_conf
+end
+
 Sidekiq.configure_server do |config|
+
+  config.redis = redis_conf
   config.on(:startup) do
 
     if ENV.key?('UPDATE_BANK_STATEMENTS_INTERVAL')
