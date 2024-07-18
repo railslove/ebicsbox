583cf29982cf81cc4f6ad612853e0ecc4ccd9c00
Jan Zaydowicz <jan.zaydowicz@railslove.com>
2024-07-18 14:10:50 +0200

    test: add healthcheck test

---
+++ spec/apis/healthcheck_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+
+require "spec_helper"
+
+module Box
+  RSpec.describe Apis::Healthcheck do
+    describe "GET /health" do
+      it "returns a successful response" do
+        get "/health"
+        expect_status 200
+      end
+
+      it "returns the expected response body" do
+        get "/health"
+        expect_json "status", "ok"
+      end
+    end
+  end
+end
