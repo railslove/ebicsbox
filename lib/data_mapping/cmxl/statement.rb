9336a5ca92e0f8102616f704f0769271d5e65c60
Jan Zaydowicz <jan.zaydowicz@railslove.com>
2025-02-10 22:27:36 +0100

    refactor: add statement mapping class
    create a unified custom DSL

---
+++ lib/data_mapping/cmxl/statement.rb
@@ -0,0 +1,23 @@
+module DataMapping
+  module Cmxl
+    class Statement
+      attr_reader :raw_bank_statement
+
+      delegate :account_identification,
+        :blank?,
+        :closing_or_intermediary_balance,
+        :opening_or_intermediary_balance,
+        :source,
+        :transactions,
+        to: :raw_bank_statement
+
+      def initialize(raw_bank_statement)
+        @raw_bank_statement = raw_bank_statement
+      end
+
+      def sequence
+        raw_bank_statement.legal_sequence_number
+      end
+    end
+  end
+end
